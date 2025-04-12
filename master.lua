ab8d3cb8d4e020884e118bc5c24bcdb4d087dfa5
decrusader <samdewaele10@gmail.com>
2025-04-12 15:57:13 +0200

    Update master.lua

--- master.lua
+++ master.lua
@@ -1,35 +1,81 @@
--- master.lua (op de master computer)
+-- Master Computer Script
+local activeTurtles = {}  -- Turtles die actief zijn (we gaan een lijst gebruiken om te tracken)
+local programName = "turtleProgram"  -- Naam van het te verzenden programma
 
--- Functie om te controleren of een bestand bestaat
-function fileExists(filename)
-    local file = fs.open(filename, "r")
-    if file then
-        file.close()
-        return true
-    else
-        return false
+-- Functie om alle turtles te verkrijgen in het netwerk
+function getTurtles()
+    local ids = {}
+    local termList = peripheral.getNames()
+    
+    -- Zoek naar alle turtles
+    for _, id in ipairs(termList) do
+        if peripheral.getType(id) == "turtle" then
+            table.insert(ids, id)
+        end
+    end
+    return ids
+end
+
+-- Functie om turtles te stoppen
+function stopTurtles()
+    for _, id in ipairs(activeTurtles) do
+        rednet.send(id, "stop")
     end
 end
 
--- Bestand en turtle ID
-local filename = "mine.lua"      -- Het bestand dat je wilt versturen
-local turtleId = 1               -- Vervang dit met de juiste turtle ID (meestal het ID van de turtle)
-
--- Controleer of het bestand bestaat
-if fileExists(filename) then
-    -- Open het bestand en lees de inhoud
-    local file = fs.open(filename, "r")
-    local fileContent = file.readAll()
-    file.close()
-
-    -- Verbind met de turtle via rednet
-    rednet.open("left")  -- Zorg ervoor dat je de juiste kant gebruikt voor je modem
-
-    -- Verstuur het bestand naar de turtle
-    print("Verzend bestand " .. filename .. " naar turtle " .. turtleId)
-    rednet.send(turtleId, {filename, fileContent}, "fileTransferProtocol")
-
-    print("Bestand succesvol verzonden naar turtle.")
-else
-    print("Het bestand " .. filename .. " bestaat niet.")
+-- Functie om turtles weer te starten
+function startTurtles()
+    for _, id in ipairs(activeTurtles) do
+        rednet.send(id, "go")
+    end
 end
+
+-- Functie om een programma naar alle turtles te verzenden
+function sendProgramToTurtles()
+    local program = fs.open(programName, "r")
+    local programCode = program.readAll()
+    program.close()
+    
+    -- Versleutelen van het programma (Base64)
+    local encodedCode = textutils.serializeToBase64(programCode)
+    
+    for _, id in ipairs(activeTurtles) do
+        rednet.send(id, "update " .. encodedCode)
+    end
+end
+
+-- UI om het aantal actieve turtles te tonen
+function showUI()
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("Aantal actieve turtles: " .. #activeTurtles)
+end
+
+-- Commando-ontvangst loop
+function handleCommands()
+    while true do
+        local event, param = os.pullEvent("rednet_message")
+        
+        if event == "rednet_message" then
+            local msg, sender = param, sender
+            
+            if msg == "stop" then
+                -- Turtle is gestopt
+                activeTurtles[sender] = nil
+                showUI()
+            elseif msg == "go" then
+                -- Turtle is weer actief
+                table.insert(activeTurtles, sender)
+                showUI()
+            elseif msg == "verstuur/" .. programName then
+                -- Het programma wordt geüpdatet en naar de turtles gestuurd
+                sendProgramToTurtles()
+            end
+        end
+    end
+end
+
+-- Start het commando handler
+getTurtles()
+showUI()
+handleCommands()
