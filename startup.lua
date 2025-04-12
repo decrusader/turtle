aae09a3284cc2ac449e855a221f80d5541c26f74
decrusader <samdewaele10@gmail.com>
2025-04-12 17:15:24 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,25 +1,50 @@
--- Turtle code
-local function executeCommand(command)
-    if command == "stop" then
-        -- Verhindert de turtle van werken (onbeweeglijk maken)
-        os.pullEvent("monitor_touch")  -- Verhindert dat de turtle verder werkt
-    elseif command == "go" then
-        -- Zet de turtle aan om weer te werken
-        print("Turtle geactiveerd")
-    elseif string.match(command, "run") then
-        local programName = string.match(command, "run (.+)")
-        if programName and fs.exists("/rom/programs/" .. programName) then
-            -- Voer het programma uit
-            shell.run("/rom/programs/" .. programName)
-            print("Programma " .. programName .. " uitgevoerd.")
-        else
-            print("Programma niet gevonden.")
-        end
-    end
+-- Controleer of modem links zit
+if not peripheral.getType("left") or peripheral.getType("left") ~= "modem" then
+    print("Geen modem aan de linkerkant gevonden. Programma gestopt.")
+    return
 end
 
--- Luister naar commando's
+-- Open rednet via linker modem
+rednet.open("left")
+print("Rednet geopend via linker modem.")
+
+local locked = false
+
+-- Hoofdlus
 while true do
-    -- Luister naar commando's van de master computer
-    os.pullEvent("monitor_touch")
+    local id, msg = rednet.receive()
+
+    if msg == "ping" then
+        rednet.send(id, "pong")
+
+    elseif msg == "stop" then
+        locked = true
+        print("Turtle is vergrendeld.")
+
+    elseif msg == "go" then
+        locked = false
+        print("Turtle is geactiveerd.")
+
+    elseif string.sub(msg, 1, 7) == "delete:" then
+        local name = string.sub(msg, 8)
+        if fs.exists(name) then
+            fs.delete(name)
+            print("Programma verwijderd: " .. name)
+        end
+
+    elseif string.sub(msg, 1, 8) == "program:" then
+        local rest = string.sub(msg, 9)
+        local name, code = rest:match("([^:]+):(.+)")
+        if name and code then
+            local file = fs.open(name, "w")
+            file.write(code)
+            file.close()
+            print("Programma opgeslagen als: " .. name)
+        end
+
+    elseif not locked then
+        print("Ontvangen onbekend commando (geen actie ondernomen).")
+    else
+        print("Turtle is gelockt. Geen acties toegestaan.")
+    end
 end
