81512744e11f308d0f16fa0bb1c3883a9155e27e
decrusader <samdewaele10@gmail.com>
2025-04-12 17:00:32 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -2,26 +2,20 @@
 local modemSide = "left"  -- Pas dit aan als de modem aan een andere kant is aangesloten
 rednet.open(modemSide)    -- Open de rednet modem
 
-local programName = "turtleProgram"  -- Het programma dat de master verzendt
-local password = "geheim"  -- Het wachtwoord voor beveiliging
-
--- Functie om om het wachtwoord te vragen
-function askForPassword()
-    term.clear()
-    term.setCursorPos(1, 1)
-    write("Voer het wachtwoord in: ")
-    local input = read()
-    if input == password then
-        return true
-    else
-        print("Wachtwoord incorrect!")
-        return false
-    end
+-- Functie om turtle te registreren
+function registerTurtle()
+    rednet.broadcast("go")  -- Stuur bericht naar Master Computer om turtle te registreren
 end
 
--- Altijd wachten op commando van de master computer
+-- Wacht op commando om te stoppen
 while true do
+    -- Registreer de turtle bij opstarten
+    registerTurtle()
+    
+    -- Wacht op berichten van de Master Computer
     local _, sender, _, _, message = os.pullEvent("rednet_message")
+    
+    print("Turtle ontving bericht: " .. message)  -- Debugging, toont het bericht dat ontvangen wordt
     
     if message == "stop" then
         -- Stop de turtle
@@ -30,34 +24,9 @@ while true do
         print("Turtle gestopt. Wacht op opdracht.")
         break
     elseif message == "go" then
-        -- Start de turtle weer
+        -- De turtle blijft actief
         term.clear()
         term.setCursorPos(1, 1)
-        print("Turtle gestart! Begin met werken.")
-        
-        -- Wacht op een geüpdate versie van het programma
-        while true do
-            local _, sender, _, _, message = os.pullEvent("rednet_message")
-            
-            if string.sub(message, 1, 7) == "update " then
-                -- Ontvang en decodeer de versleutelde code
-                local encodedCode = string.sub(message, 8)
-                local decodedCode = textutils.unserializeFromBase64(encodedCode)
-                
-                -- Sla het gedecodeerde bestand op
-                local file = fs.open(programName, "w")
-                file.write(decodedCode)
-                file.close()
-                
-                -- Voer het programma uit
-                term.clear()
-                term.setCursorPos(1, 1)
-                print("Programma bijgewerkt! Het programma wordt uitgevoerd.")
-                
-                -- Voer het gedecodeerde programma uit
-                load(decodedCode)()
-                break
-            end
-        end
+        print("Turtle is actief.")
     end
 end
