aed1cbc0a022e547d5f19aaedbe78756be48ec12
decrusader <samdewaele10@gmail.com>
2025-04-13 14:20:47 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,59 +1,55 @@
--- main.lua
-
--- Controleer of modem links zit
+-- Controleer of modem aan de linkerkant zit
 if peripheral.getType("left") ~= "modem" then
     print("Geen modem aan de linkerkant gevonden. Programma gestopt.")
     return
 end
 
+-- Open rednet via linker modem
 rednet.open("left")
 
--- Bestandsnaam voor lock status
+-- Bestandspad voor lock status
 local LOCK_FILE = "locked_status.txt"
+local locked = false
 
--- Laad status
+-- Laad de vergrendelingsstatus uit bestand
 local function loadLockStatus()
     if fs.exists(LOCK_FILE) then
-        local f = fs.open(LOCK_FILE, "r")
-        local data = textutils.unserialize(f.readAll())
-        f.close()
-        return data == true
+        local file = fs.open(LOCK_FILE, "r")
+        local data = file.readAll()
+        file.close()
+        return textutils.unserialize(data) or false
+    else
+        return false
     end
-    return false
 end
 
--- Sla status op
-local function saveLockStatus(state)
-    local f = fs.open(LOCK_FILE, "w")
-    f.write(textutils.serialize(state))
-    f.close()
+-- Sla de vergrendelingsstatus op
+local function saveLockStatus(status)
+    local file = fs.open(LOCK_FILE, "w")
+    file.write(textutils.serialize(status))
+    file.close()
 end
 
--- Blokkeer turtle indien vergrendeld
-local locked = loadLockStatus()
-
-if locked then
-    print("Turtle is geblokkeerd. Werking onmogelijk.")
-    os.sleep(2)
-    return
-end
-
--- Voer een programma veilig uit
+-- Voer programma uit als turtle niet geblokkeerd is
 local function runProgram(name)
-    if locked then return end
+    if locked then
+        print("Kan programma niet uitvoeren. Turtle is geblokkeerd.")
+        return
+    end
 
     print("Start programma: " .. name)
     local success, err = pcall(function()
         shell.run(name)
     end)
+
     if not success then
         print("Fout bij uitvoeren van '" .. name .. "': " .. tostring(err))
     else
-        print("Programma voltooid.")
+        print("Programma '" .. name .. "' is afgerond.")
     end
 end
 
--- Luister naar rednet
+-- Verwerk rednet berichten
 local function listenForRednet()
     while true do
         local id, msg = rednet.receive()
@@ -62,62 +58,70 @@ local function listenForRednet()
             rednet.send(id, "pong")
 
         elseif msg == "stop" then
-            if not locked then
-                locked = true
-                saveLockStatus(true)
-                print("Turtle geblokkeerd door master. Uitvoering wordt gestopt.")
-                os.sleep(2)
-                return -- Stop het programma (wordt herstart door startup.lua)
-            end
+            locked = true
+            saveLockStatus(true)
+            print("Turtle is nu geblokkeerd. Geen programma's kunnen worden uitgevoerd.")
 
         elseif msg == "start" then
-            if locked then
-                locked = false
-                saveLockStatus(false)
-                print("Turtle opnieuw geactiveerd.")
-            end
-
-        elseif msg == "destruct" then
-            print("Turtle permanent uitgeschakeld.")
-            os.sleep(2)
-            os.shutdown()
+            locked = false
+            saveLockStatus(false)
+            print("Turtle is opnieuw geactiveerd.")
 
         elseif msg:sub(1, 7) == "delete:" then
-            local file = msg:sub(8)
-            if fs.exists(file) then fs.delete(file) end
+            local name = msg:sub(8)
+            if fs.exists(name) then
+                fs.delete(name)
+                print("Bestand '" .. name .. "' verwijderd.")
+            end
 
         elseif msg:sub(1, 8) == "program:" then
             local rest = msg:sub(9)
             local name, code = rest:match("([^:]+):(.+)")
             if name and code then
-                local f = fs.open(name, "w")
-                f.write(code)
-                f.close()
-                if not locked then runProgram(name) end
+                local file = fs.open(name, "w")
+                file.write(code)
+                file.close()
+                print("Programma '" .. name .. "' opgeslagen.")
+                if not locked then
+                    runProgram(name)
+                end
             end
 
         elseif not locked then
             runProgram(msg)
+        else
+            print("Ontvangen commando genegeerd. Turtle is geblokkeerd.")
         end
     end
 end
 
--- Lokale invoer (alleen als niet geblokkeerd)
-local function listenForInput()
+-- Luister naar keyboard input voor lokale programma-oproep
+local function listenForKeyboard()
     while true do
         term.setCursorBlink(true)
         io.write("> ")
         local input = read()
         term.setCursorBlink(false)
 
-        if input and input ~= "" and not locked then
-            runProgram(input)
+        if input ~= "" then
+            if not locked then
+                runProgram(input)
+            else
+                print("Turtle is geblokkeerd. Voer eerst 'start' uit via master.")
+            end
         end
     end
 end
 
--- Start alles tegelijk
+-- Laad initiële status bij opstart
+locked = loadLockStatus()
+
+if locked then
+    print("Turtle is geblokkeerd. Wacht op 'start' commando via master...")
+end
+
+-- Start rednet en keyboard listeners
 parallel.waitForAll(
     listenForRednet,
-    listenForInput
+    listenForKeyboard
 )
