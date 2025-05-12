00fc0f9cb3316ea247a4de7fd852aa7a9691cb2e
legomaster1486 <152879070+legomaster1486@users.noreply.github.com>
2025-05-12 16:06:00 +0200

    inputs configured

--- suicide_bomb.lua
+++ suicide_bomb.lua
@@ -1,3 +1,15 @@
-current = { ... }
-for #current < 4 then
-  local cx
+coords = { ... }
+for #coords < 8 then
+  print("Gebruik <x> <y> <z> <x> <y> <z> <as> <+/->")
+  return
+end
+
+local cx = coords[1]
+local cy = coords[2]
+local cz = coords[3]
+local tx = coords[4]
+local ty = coords[5]
+local tz = coords[6]
+local as = coords[7]
+local zin = coords[8]
+
