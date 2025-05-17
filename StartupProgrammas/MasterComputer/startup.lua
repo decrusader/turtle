21469c93ab435dc852e03fa3030703781be96f7b
decrusader <samdewaele10@gmail.com>
2025-05-17 16:19:01 +0200

    Update startup.lua

--- StartupProgrammas/MasterComputer/startup.lua
+++ StartupProgrammas/MasterComputer/startup.lua
@@ -1,6 +1,6 @@
 -- === CONFIG ===
 local programs = {
-    {name = "Master", url = "https://raw.githubusercontent.com/decrusader/turtle/refs/heads/main/StartupProgrammas/MasterComputer/master.lua?token=GHSAT0AAAAAADCDTGAOKAXSEKGZUXYKFCMM2BITFBQ"},
+    {name = "Master", url = "https://raw.githubusercontent.com/decrusader/turtle/refs/heads/main/StartupProgrammas/MasterComputer/master.lua?token=GHSAT0AAAAAADCDTGAPVY6G7X4ELAHVLLJK2BITLSA"},
     {name = "Music", url = "https://raw.githubusercontent.com/decrusader/turtle/refs/heads/main/music.lua?token=GHSAT0AAAAAADCDTGAP3E75KDWSEQ7A5NJS2BITHIQ"},
     {name = "Debug Status Editor", url = "https://raw.githubusercontent.com/decrusader/turtle/refs/heads/main/StartupProgrammas/MasterComputer/debug_status.lua?token=GHSAT0AAAAAADCDTGAPIRUGXSGXPRF3Z4FO2BITJVQ"} -- <- Replace with your real raw URL
 }
