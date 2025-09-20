30e0578d25b02bc6c0d359922e6361cb4bc4932c
decrusader <samdewaele10@gmail.com>
2025-09-20 15:33:37 +0200

    Delete ExtraMods directory

--- ExtraMods/AutoFoodGiver.lua
+++
@@ -1,9 +0,0 @@
-local manager = peripheral.find("inventoryManager")
- 
-print("where is the chest?")
-local x = read()
-i = 0
- 
-repeat
-manager.addItemToPlayer(x, {name="minecraft:cooked_beef", toSlot=0, count=1})
-until i == 1
