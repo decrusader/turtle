025c010ba6b192380e453ab59566b8e5631c5ddf
decrusader <samdewaele10@gmail.com>
2025-09-20 16:04:50 +0200

    Update startup.lua

--- OS/startup.lua
+++ OS/startup.lua
@@ -1,8 +1,32 @@
 -- startup.lua
--- Start CoreLogic OS en speel animatie af
+-- CoreLogic OS startup met automatische bestand-download
 
-local animation = dofile("animation.lua")  -- laad animation.lua
-animation.play()                            -- speel animatie af
+-- Functie om een bestand te downloaden
+local function downloadFile(url, filename)
+    print("Downloading "..filename.."...")
+    local response = http.get(url)
+    if response then
+        local content = response.readAll()
+        response.close()
+        local f = fs.open(filename, "w")
+        f.write(content)
+        f.close()
+        print(filename.." downloaded successfully!")
+    else
+        print("Fout: kon "..filename.." niet downloaden!")
+    end
+end
 
--- Hier kan je je eigen main menu of OS starten
--- bijvoorbeeld: dofile("main.lua")
+-- Controleer of animation.lua bestaat, anders downloaden
+if not fs.exists("animation.lua") then
+    -- Vervang deze URL door de raw link van jouw animation.lua
+    local animationURL = "https://raw.githubusercontent.com/gebruikersnaam/repo/main/animation.lua"
+    downloadFile(animationURL, "animation.lua")
+end
+
+-- Laad en speel animatie af
+local animation = dofile("animation.lua")
+animation.play()
+
+-- Hier kan je je eigen OS starten, bv:
+-- dofile("main.lua")
