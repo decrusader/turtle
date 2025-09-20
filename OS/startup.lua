b40c0225d933ddc5acfa75de387f921b7e89c6fe
decrusader <samdewaele10@gmail.com>
2025-09-20 16:05:14 +0200

    Update startup.lua

--- OS/startup.lua
+++ OS/startup.lua
@@ -20,7 +20,7 @@ end
 -- Controleer of animation.lua bestaat, anders downloaden
 if not fs.exists("animation.lua") then
     -- Vervang deze URL door de raw link van jouw animation.lua
-    local animationURL = "https://raw.githubusercontent.com/gebruikersnaam/repo/main/animation.lua"
+    local animationURL = "https://github.com/decrusader/turtle/blob/main/OS/animation.lua"
     downloadFile(animationURL, "animation.lua")
 end
 
