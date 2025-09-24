2ce0ad22366ebe9679ad4ffccc457250be14c27a
decrusader <samdewaele10@gmail.com>
2025-09-24 16:42:18 +0200

    Update chat.lua

--- OS/chat.lua
+++ OS/chat.lua
@@ -1,8 +1,5 @@
--- chat.lua
-
 local modem = peripheral.find("modem") or error("Geen modem gevonden!", 0)
 
--- Kies kanaal
 term.clear()
 term.setCursorPos(1,1)
 write("Kies een channel (0-65535): ")
@@ -11,24 +8,23 @@ local channel = tonumber(read()) or error("Ongeldig channel", 0)
 modem.open(channel)
 print("Chat gestart op channel " .. channel)
 print("Typ berichten. Ctrl+T om te stoppen.")
+sleep(3)
+term.clear
 
--- Stuurfunctie
 local function sendMessage(text)
     local id = os.getComputerID()
     modem.transmit(channel, channel, ("id: %d %s"):format(id, text))
 end
 
--- Ontvangstloop
 local function receiveLoop()
     while true do
         local event, side, ch, replyCh, message, distance = os.pullEvent("modem_message")
         if ch == channel then
-            print(message)
+            print("id: %d %s",message)
         end
     end
 end
 
--- Invoerlus
 local function inputLoop()
     while true do
         local text = read()
@@ -38,16 +34,13 @@ local function inputLoop()
     end
 end
 
--- Stoploop Ctrl+T
 local function stopLoop()
     while true do
         local event, key = os.pullEvent("key")
         if key == keys.t and keyboard.isControlDown() then
-            print("Ctrl+T gedrukt, programma stopt")
             return
         end
     end
 end
 
--- Parallel uitvoeren
 parallel.waitForAny(receiveLoop, inputLoop, stopLoop)
